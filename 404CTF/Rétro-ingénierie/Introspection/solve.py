4feeb32a30fef2fceb38da376e84961bbcf928a9
Garo Benjamin <47636025+bgaro@users.noreply.github.com>
2023-06-29 10:23:07 +0200

    Update solve.py

--- 404CTF/Rétro-ingénierie/Introspection/solve.py
+++ 404CTF/Rétro-ingénierie/Introspection/solve.py
@@ -8,16 +8,20 @@ def main():
     offset = offset - len_of_small_array
 
     # Skip Audio
-    offset = offset - 16
+    offset = remove_padding(result, offset)
+    offset = offset - 3
     len_of_small_array = int.from_bytes(result[offset : offset + 3], "little")
     offset = offset - len_of_small_array
-
-    offset = offset - 4
+    # small array
+    offset = remove_padding(result, offset)
+    offset = offset - 2
     len_of_small_array = int.from_bytes(result[offset : offset + 2], "little")
     offset = offset - len_of_small_array
     small_array = result[offset : offset + len_of_small_array]
 
-    offset = offset - 24
+    # big array
+    offset = remove_padding(result, offset)
+    offset = offset - 3
     len_of_big_array = int.from_bytes(result[offset : offset + 3], "little")
     offset = offset - len_of_big_array
     big_array = result[offset : offset + len_of_big_array]
@@ -28,36 +32,25 @@ def main():
             ((-1 + small_array[i % len_of_small_array] + 1 ^ big_array[i]) % 256)
         )
 
-    j = 1
     while result[:4] == b"\x7f\x45\x4c\x46":
         try:
             tmp_result = result
-            j += 1
-            print(j)
             offset = result.find(b"\x47\x43\x43\x3a") - 4
             len_of_small_array = int.from_bytes(result[offset : offset + 4], "little")
 
             # Avoid padding
-            for k in range(100):
-                if result[offset - 1] == 0x00:
-                    offset = offset - 1
-                else:
-                    break
+            offset = remove_padding(result, offset)
             small_array = result[offset - len_of_small_array : offset]
             offset = offset - len_of_small_array
 
             # Avoid padding
-            for k in range(100):
-                if result[offset - 1] == 0x00:
-                    offset = offset - 1
-                else:
-                    break
-            len_of_big_array = int.from_bytes(result[offset - 3 : offset + 1], "little")
-            if len_of_big_array > 2601128:
-                len_of_big_array = int.from_bytes(
-                    result[offset - 2 : offset + 2], "little"
-                )
+            offset = remove_padding(result, offset)
 
+            # 2 cases here, either 2 or 3 bytes for the length of the big array
+            len_of_big_array = int.from_bytes(result[offset - 3 : offset], "little")
+            # We know that the length of the big array will be smaller than 2601128 (length of the big array in the first iteration)
+            if len_of_big_array > 2601128:
+                len_of_big_array = int.from_bytes(result[offset - 2 : offset], "little")
                 offset = offset - 2
             else:
                 offset = offset - 3
@@ -77,7 +70,12 @@ def main():
     print(result[:4])
     with open("result", "wb") as f:
         f.write(tmp_result)
-        print(j)
+
+
+def remove_padding(result, offset):
+    while result[offset - 1] == 0x00:
+        offset = offset - 1
+    return offset
 
 
 if __name__ == "__main__":
